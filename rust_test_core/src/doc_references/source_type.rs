113cef486d397635f293974793efcee20193ad28
vdermichev <v.dermichev@tquality.ru>
2026-01-07 04:24:00 +0100

    Add badges to README and fix typo in proc macro documentation

--- rust_test_core/src/doc_references/source_type.rs
+++ rust_test_core/src/doc_references/source_type.rs
@@ -11,13 +11,19 @@ pub enum SourceType<T: DeserializeOwned>
 {
     /// # Example
     /// ```rust
+    /// # use rust_test_core::SourceType;
+    /// # use serde::Deserialize;
+    /// # #[derive(Deserialize)]
+    /// # struct User { name: String, age: u32 }
+    /// # let user_source: SourceType<User> =
     /// // Type inferred from the function signature:
-    /// #[test_case_source(JsonFile("data.json"))]
-    /// fn my_test(data: User) { ... }
-    ///
-    /// // Type explicitly provided:
-    /// #[test_case_source(JsonFile::<Vec<Users>>("data.json"))]
-    /// fn my_test<T: Debug>(data: T) { ... }
+    /// SourceType::JsonFile("data.json")
+    /// # ;
+    /// # let users_source =
+    /// // Type explicitly provided,
+    /// // can also be used as JsonFile::<Vec<User>>("data.json")
+    /// SourceType::<Vec<User>>::JsonFile("data.json")
+    /// # ;
     /// ```
     JsonFile(&'static str),
     #[doc(hidden)]
